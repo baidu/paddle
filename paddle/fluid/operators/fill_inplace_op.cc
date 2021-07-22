baidu/PaddlePaddle

add fill_ backward

paddle/fluid/operators/fill_inplace_op.cc
@@ -24,8 +24,13 @@ class Fill_OpMaker : public framework::OpProtoAndCheckerMaker {
                 Fill an tensor inplace with `value` and `shape`. The type of the tensor is specify by
                 `dtype`.
                 )DOC");
-    AddInput("X", "(LoDTensor) The input tensor.");
-    AddInput("value", "The float values of tensor, whose dim is one");
+    AddInput("X", "(Tensor) The input tensor.");
+    AddOutput("Out",
+              "Tensor, the clipped tensor, with the same shape and data type "
+              "as input(x)");
+    AddInput(
+        "value",
+        "The float values of tensor, whose dim is one, and no need of grad");
   }
 };
 
@@ -35,6 +40,10 @@ class Fill_Op : public framework::OperatorWithKernel {
 
   void InferShape(framework::InferShapeContext *context) const override {
     OP_INOUT_CHECK(context->HasInput("X"), "Input", "X", "Fill_");
+    OP_INOUT_CHECK(context->HasOutput("Out"), "Output", "Out", "Fill_");
+    auto x_dims = context->GetInputDim("X");
+    context->SetOutputDim("Out", x_dims);
+    // context->ShareLoD("X", /*->*/ "Out");
   }
 
  protected:
@@ -54,24 +63,80 @@ template <typename T>
 class Fill_Kernel : public framework::OpKernel<T> {
  public:
   void Compute(const paddle::framework::ExecutionContext &ctx) const override {
-    auto *in = ctx.Input<framework::LoDTensor>("X");
-    auto *value = ctx.Input<framework::LoDTensor>("value");
-
-    platform::CPUPlace cpu;
-    auto *data = const_cast<framework::LoDTensor *>(in)->mutable_data<T>(cpu);
+    auto *in = ctx.Input<framework::Tensor>("X");
+    auto *value = ctx.Input<framework::Tensor>("value");
+    auto *out = ctx.Output<framework::Tensor>("Out");
+    T *out_data = out->mutable_data<T>(ctx.GetPlace());
     auto fill_val = *(value->data<T>());
-    std::fill(data, data + in->numel(), fill_val);
+    std::fill(out_data, out_data + in->numel(), fill_val);
   }
 };
 
+class Fill_GradOp : public framework::OperatorWithKernel {
+ public:
+  using framework::OperatorWithKernel::OperatorWithKernel;
+
+  void InferShape(framework::InferShapeContext *ctx) const override {
+    OP_INOUT_CHECK(ctx->HasInput(framework::GradVarName("Out")), "Input",
+                   "Out@GRAD", "mul");
+    auto x_dims = ctx->GetInputDim(framework::GradVarName("Out"));
+    auto x_grad_name = framework::GradVarName("X");
+    if (ctx->HasOutput(x_grad_name)) {
+      ctx->SetOutputDim(x_grad_name, x_dims);
+    }
+  }
+};
+
+template <typename T>
+class Fill_GradOpMaker : public framework::SingleGradOpMaker<T> {
+ public:
+  using framework::SingleGradOpMaker<T>::SingleGradOpMaker;
+
+ protected:
+  void Apply(GradOpPtr<T> retv) const override {
+    retv->SetType(this->ForwardOpType() + "_grad");
+    retv->SetInput(framework::GradVarName("Out"), this->OutputGrad("Out"));
+    retv->SetOutput(framework::GradVarName("X"), this->InputGrad("X"));
+  }
+};
+
+template <typename T>
+class Fill_GradKernel : public framework::OpKernel<T> {
+ public:
+  void Compute(const paddle::framework::ExecutionContext &ctx) const override {
+    auto *dx = ctx.Output<framework::Tensor>(framework::GradVarName("X"));
+    if (dx) {
+      auto *data = dx->mutable_data<T>(ctx.GetPlace());
+      std::fill(data, data + dx->numel(), T(0));
+    }
+  }
+};
+
+DECLARE_INPLACE_OP_INFERER(Fill_OpInplaceInferer, {"X", "Out"});
+DECLARE_INPLACE_OP_INFERER(Fill_GradInplaceInferer,
+                           {framework::GradVarName("Out"),
+                            framework::GradVarName("X")});
 }  // namespace operators
 }  // namespace paddle
 namespace ops = paddle::operators;
-REGISTER_OPERATOR(
-    fill_inplace, ops::Fill_Op, ops::Fill_OpMaker, ops::Fill_OpVarTypeInference,
-    paddle::framework::EmptyGradOpMaker<paddle::framework::OpDesc>,
-    paddle::framework::EmptyGradOpMaker<paddle::imperative::OpBase>);
+
+REGISTER_OPERATOR(fill_inplace, ops::Fill_Op, ops::Fill_OpMaker,
+                  ops::Fill_OpVarTypeInference,
+                  ops::Fill_GradOpMaker<paddle::framework::OpDesc>,
+                  ops::Fill_GradOpMaker<paddle::imperative::OpBase>,
+                  ops::Fill_OpInplaceInferer);
+
+REGISTER_OPERATOR(fill_inplace_grad, ops::Fill_GradOp,
+                  ops::Fill_GradInplaceInferer);
+
 REGISTER_OP_CPU_KERNEL(fill_inplace, ops::Fill_Kernel<float>,
                        ops::Fill_Kernel<double>, ops::Fill_Kernel<int64_t>,
                        ops::Fill_Kernel<int>,
-                       ops::Fill_Kernel<paddle::platform::float16>);
+                       ops::Fill_Kernel<paddle::platform::float16>,
+                       ops::Fill_Kernel<bool>);
+
+REGISTER_OP_CPU_KERNEL(fill_inplace_grad, ops::Fill_GradKernel<float>,
+                       ops::Fill_GradKernel<double>,
+                       ops::Fill_GradKernel<int64_t>, ops::Fill_GradKernel<int>,
+                       ops::Fill_GradKernel<paddle::platform::float16>,
+                       ops::Fill_GradKernel<bool>);

paddle/fluid/operators/fill_inplace_op.cu
@@ -23,12 +23,14 @@ namespace operators {
 using Tensor = framework::Tensor;
 using CUDADeviceContext = paddle::platform::CUDADeviceContext;
 
+__constant__ int64_t convar[1];
+
 template <typename T>
-__global__ void fill_constant_kernel(const int64_t featuresize, T* in_data,
-                                     const T* value) {
+__global__ void fill_constant_kernel(const int64_t featuresize, T* in_data) {
+  T* temp = reinterpret_cast<T*>(convar);
   for (int idx = blockIdx.x * featuresize + threadIdx.x;
        idx < (blockIdx.x + 1) * featuresize; idx += blockDim.x) {
-    in_data[idx] = *value;
+    in_data[idx] = *temp;
   }
 }
 
@@ -41,18 +43,40 @@ class Fill_CUDAKernel : public framework::OpKernel<T> {
 #else
     const int64_t kMaxBlockDim = 512;
 #endif
-
-    auto* in = ctx.Input<Tensor>("X");
+    auto* x = ctx.Input<Tensor>("X");
     auto* value = ctx.Input<Tensor>("value");
-
-    auto* in_data = in->data<T>();
-    const auto x_dims = in->dims();
+    auto* out = ctx.Output<Tensor>("Out");
+    T* out_data = out->mutable_data<T>(ctx.GetPlace());
+    T* fill_value = const_cast<T*>(value->data<T>());
+    cudaMemcpyToSymbol(convar, fill_value, sizeof(T));
+    auto x_dims = x->dims();
     auto matrix_dim = framework::flatten_to_2d(x_dims, 1);
     int64_t batchsize = static_cast<int64_t>(matrix_dim[0]);
     int64_t featuresize = static_cast<int64_t>(matrix_dim[1]);
     int64_t kBlockDim = std::min(featuresize, kMaxBlockDim);
-    fill_constant_kernel<T><<<batchsize, kBlockDim, 0>>>(
-        featuresize, const_cast<T*>(in_data), value->data<T>());
+    fill_constant_kernel<T><<<batchsize, kBlockDim, 0>>>(featuresize, out_data);
+  }
+};
+
+template <typename T>
+class Fill_GradCUDAKernel : public framework::OpKernel<T> {
+ public:
+  void Compute(const framework::ExecutionContext& ctx) const override {
+#ifdef __HIPCC__
+    const int64_t kMaxBlockDim = 256;
+#else
+    const int64_t kMaxBlockDim = 512;
+#endif
+    auto* dx = ctx.Output<framework::Tensor>(framework::GradVarName("X"));
+    auto* in_data = dx->mutable_data<T>(ctx.GetPlace());
+    T temp = T(0);
+    cudaMemcpyToSymbol(convar, &temp, sizeof(T));
+    const auto x_dims = dx->dims();
+    auto matrix_dim = framework::flatten_to_2d(x_dims, 1);
+    int64_t batchsize = static_cast<int64_t>(matrix_dim[0]);
+    int64_t featuresize = static_cast<int64_t>(matrix_dim[1]);
+    int64_t kBlockDim = std::min(featuresize, kMaxBlockDim);
+    fill_constant_kernel<T><<<batchsize, kBlockDim, 0>>>(featuresize, in_data);
   }
 };
 
@@ -68,3 +92,10 @@ REGISTER_OP_CUDA_KERNEL(fill_inplace, ops::Fill_CUDAKernel<float>,
                         ops::Fill_CUDAKernel<int>,
                         ops::Fill_CUDAKernel<int64_t>,
                         ops::Fill_CUDAKernel<bool>);
+
+REGISTER_OP_CUDA_KERNEL(fill_inplace_grad, ops::Fill_GradCUDAKernel<float>,
+                        ops::Fill_GradCUDAKernel<double>,
+                        ops::Fill_GradCUDAKernel<int>,
+                        ops::Fill_GradCUDAKernel<int64_t>,
+                        ops::Fill_GradCUDAKernel<plat::float16>,
+                        ops::Fill_GradCUDAKernel<bool>);

baidu/paddle

paddle/fluid/operators/fill_inplace_op.cc
@@ -17,24 +17,33 @@ limitations under the License. */
 namespace paddle {
 namespace operators {
 
-class Fill_OpMaker : public framework::OpProtoAndCheckerMaker {
+class FillInplaceOpMaker : public framework::OpProtoAndCheckerMaker {
  public:
   void Make() override {
     AddComment(R"DOC(Fill replace operator
                 Fill an tensor inplace with `value` and `shape`. The type of the tensor is specify by
                 `dtype`.
                 )DOC");
-    AddInput("X", "(LoDTensor) The input tensor.");
-    AddInput("value", "The float values of tensor, whose dim is one");
+    AddInput("X", "(Tensor) The input tensor.");
+    AddOutput("Out",
+              "Tensor, the clipped tensor, with the same shape and data type "
+              "as input(x)");
+    AddInput(
+        "value",
+        "The float values of tensor, whose dim is one, and no need of grad");
   }
 };
 
-class Fill_Op : public framework::OperatorWithKernel {
+class FillInplaceOp : public framework::OperatorWithKernel {
  public:
   using framework::OperatorWithKernel::OperatorWithKernel;
 
   void InferShape(framework::InferShapeContext *context) const override {
-    OP_INOUT_CHECK(context->HasInput("X"), "Input", "X", "Fill_");
+    OP_INOUT_CHECK(context->HasInput("X"), "Input", "X", "FillInplace");
+    OP_INOUT_CHECK(context->HasOutput("Out"), "Output", "Out", "FillInplace");
+    auto x_dims = context->GetInputDim("X");
+    context->SetOutputDim("Out", x_dims);
+    // context->ShareLoD("X", /*->*/ "Out");
   }
 
  protected:
@@ -45,33 +54,91 @@ class Fill_Op : public framework::OperatorWithKernel {
   }
 };
 
-class Fill_OpVarTypeInference : public framework::VarTypeInference {
+class FillInplaceOpVarTypeInference : public framework::VarTypeInference {
  public:
   void operator()(framework::InferVarTypeContext *ctx) const override {}
 };
 
 template <typename T>
-class Fill_Kernel : public framework::OpKernel<T> {
+class FillInplaceKernel : public framework::OpKernel<T> {
  public:
   void Compute(const paddle::framework::ExecutionContext &ctx) const override {
-    auto *in = ctx.Input<framework::LoDTensor>("X");
-    auto *value = ctx.Input<framework::LoDTensor>("value");
-
-    platform::CPUPlace cpu;
-    auto *data = const_cast<framework::LoDTensor *>(in)->mutable_data<T>(cpu);
+    auto *in = ctx.Input<framework::Tensor>("X");
+    auto *value = ctx.Input<framework::Tensor>("value");
+    auto *out = ctx.Output<framework::Tensor>("Out");
+    T *out_data = out->mutable_data<T>(ctx.GetPlace());
     auto fill_val = *(value->data<T>());
-    std::fill(data, data + in->numel(), fill_val);
+    std::fill(out_data, out_data + in->numel(), fill_val);
   }
 };
 
+class FillInplaceGradOp : public framework::OperatorWithKernel {
+ public:
+  using framework::OperatorWithKernel::OperatorWithKernel;
+
+  void InferShape(framework::InferShapeContext *ctx) const override {
+    OP_INOUT_CHECK(ctx->HasInput(framework::GradVarName("Out")), "Input",
+                   "Out@GRAD", "mul");
+    auto x_dims = ctx->GetInputDim(framework::GradVarName("Out"));
+    auto x_grad_name = framework::GradVarName("X");
+    if (ctx->HasOutput(x_grad_name)) {
+      ctx->SetOutputDim(x_grad_name, x_dims);
+    }
+  }
+};
+
+template <typename T>
+class FillInplaceGradOpMaker : public framework::SingleGradOpMaker<T> {
+ public:
+  using framework::SingleGradOpMaker<T>::SingleGradOpMaker;
+
+ protected:
+  void Apply(GradOpPtr<T> retv) const override {
+    retv->SetType(this->ForwardOpType() + "_grad");
+    retv->SetInput(framework::GradVarName("Out"), this->OutputGrad("Out"));
+    retv->SetOutput(framework::GradVarName("X"), this->InputGrad("X"));
+  }
+};
+
+template <typename T>
+class FillInplaceGradKernel : public framework::OpKernel<T> {
+ public:
+  void Compute(const paddle::framework::ExecutionContext &ctx) const override {
+    auto *dx = ctx.Output<framework::Tensor>(framework::GradVarName("X"));
+    if (dx) {
+      auto *data = dx->mutable_data<T>(ctx.GetPlace());
+      std::fill(data, data + dx->numel(), T(0));
+    }
+  }
+};
+
+DECLARE_INPLACE_OP_INFERER(FillInplaceOpInplaceInferer, {"X", "Out"});
+DECLARE_INPLACE_OP_INFERER(FillInplaceGradInplaceInferer,
+                           {framework::GradVarName("Out"),
+                            framework::GradVarName("X")});
 }  // namespace operators
 }  // namespace paddle
 namespace ops = paddle::operators;
-REGISTER_OPERATOR(
-    fill_inplace, ops::Fill_Op, ops::Fill_OpMaker, ops::Fill_OpVarTypeInference,
-    paddle::framework::EmptyGradOpMaker<paddle::framework::OpDesc>,
-    paddle::framework::EmptyGradOpMaker<paddle::imperative::OpBase>);
-REGISTER_OP_CPU_KERNEL(fill_inplace, ops::Fill_Kernel<float>,
-                       ops::Fill_Kernel<double>, ops::Fill_Kernel<int64_t>,
-                       ops::Fill_Kernel<int>,
-                       ops::Fill_Kernel<paddle::platform::float16>);
+
+REGISTER_OPERATOR(fill_inplace, ops::FillInplaceOp, ops::FillInplaceOpMaker,
+                  ops::FillInplaceOpVarTypeInference,
+                  ops::FillInplaceGradOpMaker<paddle::framework::OpDesc>,
+                  ops::FillInplaceGradOpMaker<paddle::imperative::OpBase>,
+                  ops::FillInplaceOpInplaceInferer);
+
+REGISTER_OPERATOR(fill_inplace_grad, ops::FillInplaceGradOp,
+                  ops::FillInplaceGradInplaceInferer);
+
+REGISTER_OP_CPU_KERNEL(fill_inplace, ops::FillInplaceKernel<float>,
+                       ops::FillInplaceKernel<double>,
+                       ops::FillInplaceKernel<int64_t>,
+                       ops::FillInplaceKernel<int>,
+                       ops::FillInplaceKernel<paddle::platform::float16>,
+                       ops::FillInplaceKernel<bool>);
+
+REGISTER_OP_CPU_KERNEL(fill_inplace_grad, ops::FillInplaceGradKernel<float>,
+                       ops::FillInplaceGradKernel<double>,
+                       ops::FillInplaceGradKernel<int64_t>,
+                       ops::FillInplaceGradKernel<int>,
+                       ops::FillInplaceGradKernel<paddle::platform::float16>,
+                       ops::FillInplaceGradKernel<bool>);

paddle/fluid/operators/fill_inplace_op.cu
@@ -23,17 +23,19 @@ namespace operators {
 using Tensor = framework::Tensor;
 using CUDADeviceContext = paddle::platform::CUDADeviceContext;
 
+__constant__ int64_t convar[1];
+
 template <typename T>
-__global__ void fill_constant_kernel(const int64_t featuresize, T* in_data,
-                                     const T* value) {
+__global__ void fill_constant_kernel(const int64_t featuresize, T* in_data) {
+  T* temp = reinterpret_cast<T*>(convar);
   for (int idx = blockIdx.x * featuresize + threadIdx.x;
        idx < (blockIdx.x + 1) * featuresize; idx += blockDim.x) {
-    in_data[idx] = *value;
+    in_data[idx] = *temp;
   }
 }
 
 template <typename T>
-class Fill_CUDAKernel : public framework::OpKernel<T> {
+class FillInplaceCUDAKernel : public framework::OpKernel<T> {
  public:
   void Compute(const framework::ExecutionContext& ctx) const override {
 #ifdef __HIPCC__
@@ -41,18 +43,40 @@ class Fill_CUDAKernel : public framework::OpKernel<T> {
 #else
     const int64_t kMaxBlockDim = 512;
 #endif
-
-    auto* in = ctx.Input<Tensor>("X");
+    auto* x = ctx.Input<Tensor>("X");
     auto* value = ctx.Input<Tensor>("value");
-
-    auto* in_data = in->data<T>();
-    const auto x_dims = in->dims();
+    auto* out = ctx.Output<Tensor>("Out");
+    T* out_data = out->mutable_data<T>(ctx.GetPlace());
+    T* fill_value = const_cast<T*>(value->data<T>());
+    cudaMemcpyToSymbol(convar, fill_value, sizeof(T));
+    auto x_dims = x->dims();
     auto matrix_dim = framework::flatten_to_2d(x_dims, 1);
     int64_t batchsize = static_cast<int64_t>(matrix_dim[0]);
     int64_t featuresize = static_cast<int64_t>(matrix_dim[1]);
     int64_t kBlockDim = std::min(featuresize, kMaxBlockDim);
-    fill_constant_kernel<T><<<batchsize, kBlockDim, 0>>>(
-        featuresize, const_cast<T*>(in_data), value->data<T>());
+    fill_constant_kernel<T><<<batchsize, kBlockDim, 0>>>(featuresize, out_data);
+  }
+};
+
+template <typename T>
+class FillInplaceGradCUDAKernel : public framework::OpKernel<T> {
+ public:
+  void Compute(const framework::ExecutionContext& ctx) const override {
+#ifdef __HIPCC__
+    const int64_t kMaxBlockDim = 256;
+#else
+    const int64_t kMaxBlockDim = 512;
+#endif
+    auto* dx = ctx.Output<framework::Tensor>(framework::GradVarName("X"));
+    auto* in_data = dx->mutable_data<T>(ctx.GetPlace());
+    T temp = T(0);
+    cudaMemcpyToSymbol(convar, &temp, sizeof(T));
+    const auto x_dims = dx->dims();
+    auto matrix_dim = framework::flatten_to_2d(x_dims, 1);
+    int64_t batchsize = static_cast<int64_t>(matrix_dim[0]);
+    int64_t featuresize = static_cast<int64_t>(matrix_dim[1]);
+    int64_t kBlockDim = std::min(featuresize, kMaxBlockDim);
+    fill_constant_kernel<T><<<batchsize, kBlockDim, 0>>>(featuresize, in_data);
   }
 };
 
@@ -62,9 +86,17 @@ class Fill_CUDAKernel : public framework::OpKernel<T> {
 namespace ops = paddle::operators;
 namespace plat = paddle::platform;
 
-REGISTER_OP_CUDA_KERNEL(fill_inplace, ops::Fill_CUDAKernel<float>,
-                        ops::Fill_CUDAKernel<double>,
-                        ops::Fill_CUDAKernel<plat::float16>,
-                        ops::Fill_CUDAKernel<int>,
-                        ops::Fill_CUDAKernel<int64_t>,
-                        ops::Fill_CUDAKernel<bool>);
+REGISTER_OP_CUDA_KERNEL(fill_inplace, ops::FillInplaceCUDAKernel<float>,
+                        ops::FillInplaceCUDAKernel<double>,
+                        ops::FillInplaceCUDAKernel<plat::float16>,
+                        ops::FillInplaceCUDAKernel<int>,
+                        ops::FillInplaceCUDAKernel<int64_t>,
+                        ops::FillInplaceCUDAKernel<bool>);
+
+REGISTER_OP_CUDA_KERNEL(fill_inplace_grad,
+                        ops::FillInplaceGradCUDAKernel<float>,
+                        ops::FillInplaceGradCUDAKernel<double>,
+                        ops::FillInplaceGradCUDAKernel<int>,
+                        ops::FillInplaceGradCUDAKernel<int64_t>,
+                        ops::FillInplaceGradCUDAKernel<plat::float16>,
+                        ops::FillInplaceGradCUDAKernel<bool>);